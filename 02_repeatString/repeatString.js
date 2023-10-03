cea1bcacc3e9ea940b83b10d81e579559f55355a
JustDom223 <124432802+JustDom223@users.noreply.github.com>
2023-10-03 10:16:11 +0200

    changed the files in 01 and 02 to pass all tests

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,14 @@
-const repeatString = function() {
+const repeatString = function(string, num) {
+    let word = string
+    let outcome = ''
+    if (num < 0)
+        return 'ERROR'
 
+    while (num > 0){
+        outcome += word
+        num--;
+    };
+    return outcome
 };
 
 // Do not edit below this line
