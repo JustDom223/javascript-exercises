d6db3fcece486cee70e5eb8a79a2548a493fad70
JustDom223 <124432802+JustDom223@users.noreply.github.com>
2023-10-12 12:36:17 +0200

    Completed the palindromes then learned about chaining string manipulation

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,21 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+    const formattedString = str.toLowerCase().replace(/[^a-z0-9]/g, ''); // .replace(/[^a-z0-9]/g, ''); checkes for anything that isnt these letters or numbers and removes them
+    // const lowerString = a.toLowerCase()
+    // const strippedString = lowerString.replace(/[.,\/#!$%\^&\*;:{}=\-_`~()]/g,"");
+    // const finalString = strippedString.replace(/\s+/g,"");
+    return formattedString === formattedString.split('').reverse().join('');
+    // const newArray = finalString.split('')
+    // const reversedArray = newArray.reverse()
+    // const reversedString = reversedArray.join('')
+    // // return reversedString
+    // if(reversedString === finalString){
+    //     return true
+    // } else {
+    //     return false
+    // };
+   
 };
+
 
 // Do not edit below this line
 module.exports = palindromes;
