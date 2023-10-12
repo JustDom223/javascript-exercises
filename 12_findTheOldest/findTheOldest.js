3719a3f26b0ed4386780adc1df48395453c83e4b
JustDom223 <124432802+JustDom223@users.noreply.github.com>
2023-10-12 16:09:17 +0200

    Managed to finish the "find the oldest challange"

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,17 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+    people.forEach(person =>{
+    if(!person.yearOfDeath){
+        person.yearOfDeath = new Date().getFullYear();
+    }
+});
+    const oldest = people.sort(function(a, b){
 
-};
-
+        const lastGuy = a.yearOfDeath - a.yearOfBirth;
+        const nextGuy = b.yearOfDeath - b.yearOfBirth;
+        return lastGuy > nextGuy ? 1 : -1;
+      })
+      return oldest.pop()
+}
+    
 // Do not edit below this line
 module.exports = findTheOldest;
