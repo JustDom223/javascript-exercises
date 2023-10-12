489f171cc174a7637e12f37e2705ea293accad2a
JustDom223 <124432802+JustDom223@users.noreply.github.com>
2023-10-12 13:58:29 +0200

    completed the fibonacci number formula using an array and then pulling the final number off the end using pop

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,4 +1,13 @@
-const fibonacci = function() {
+const fibonacci = function(num) {
+    if(num == 0){
+        return 0;
+    }else if(num < 0){
+        return 'OOPS';
+    }
+    let numArray = [0,1,1]
+    for(i = 2; i<num; i++){
+        numArray.push(numArray.at(-1)+numArray.at(-2));
+    }return numArray.pop();
 
 };
 
